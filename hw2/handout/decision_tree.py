35f5d48aec0a5932f68ebe7e0b92fbc1e57799aa
Isaac Li <isaacli@andrew.cmu.edu>
2026-01-22 15:04:20 -0500

    Predictions based on a file

--- hw2/handout/decision_tree.py
+++ hw2/handout/decision_tree.py
@@ -39,6 +39,8 @@ def calculate_column_entropy(data, column_name):
     num_rows = column.shape[0]
 
     num_0, num_1 = count_ones_and_zeroes(data, column_name)
+    if num_0 == 0 or num_1 == 0: 
+        return 0 
     
     entropy = 0
 
@@ -90,6 +92,87 @@ def calculate_mutual_information(data, label_column_name, conditional_column_nam
 
     return mutual_information 
 
+def find_majority_vote(data, label_column_name): 
+    num_0, num_1 = count_ones_and_zeroes(data, label_column_name)
+
+    if num_1 >= num_0: 
+        return 1 
+    else: 
+        return 0 
+
+def train_decision_tree(data, label_column_name, current_depth, maximum_depth, used_split_set): 
+    """
+    Recursively builds a decision tree. 
+    """
+
+    if current_depth > maximum_depth: 
+        majority_vote = find_majority_vote(data, label_column_name)
+        return Node(None, majority_vote)
+    
+    num_0, num_1 = count_ones_and_zeroes(data, label_column_name) 
+    if num_0 == 0: 
+        return Node(None, 1) 
+    elif num_1 == 0: 
+        return Node(None, 0)
+
+    best_mutual_information_column = None 
+    best_mutual_information_value = None 
+    feature_column_names = data.dtype.names[:-1]
+    for feature_column_name in feature_column_names: 
+        if feature_column_name in used_split_set: 
+            continue 
+            
+        mutual_information_value = calculate_mutual_information(data, label_column_name, feature_column_name) 
+        if mutual_information_value > 0: 
+            if best_mutual_information_value == None: 
+                best_mutual_information_value = mutual_information_value 
+                best_mutual_information_column = feature_column_name 
+            else: 
+                if mutual_information_value > best_mutual_information_value: 
+                    best_mutual_information_value = mutual_information_value
+                    best_mutual_information_column = feature_column_name
+
+    if best_mutual_information_column is None: 
+        return Node(None, find_majority_vote(data)) 
+
+    used_split_set.add(best_mutual_information_column) 
+
+    newNode = Node(best_mutual_information_column, None) 
+    X_0, X_1 = split(data, best_mutual_information_column)
+    newNode.left = train_decision_tree(X_0, label_column_name, current_depth + 1, maximum_depth, used_split_set)
+    newNode.right = train_decision_tree(X_1, label_column_name, current_depth + 1, maximum_depth, used_split_set) 
+
+    used_split_set.remove(best_mutual_information_column)
+
+    return newNode 
+        
+def predict_example(node, example): 
+    if node.vote is not None: 
+        return Node.vote 
+
+    split_value = example[node.attribute]
+    if split_value == 0: 
+        return predict_example(node.left, example) 
+    else: 
+        return predict_example(node.right, example) 
+    
+def predict_file(node, file_name): 
+    file_data = np.genfromtxt(
+        file_name, 
+        delimiter = "\t", 
+        names = True, 
+        dtype = int 
+    )
+
+    file_data_without_labels = file_data[:, -1]
+
+    output_list = []
+    for row in file_data_without_labels: 
+        prediction = predict_example(node, row) 
+        output_list.append(prediction) 
+    
+    return output_list
+
 
 def print_tree(node):
     pass
@@ -117,6 +200,14 @@ if __name__ == '__main__':
     print_out = args.print_out
 
     data_holder = DataHolder(args) 
+
+    decision_tree = train_decision_tree(
+            data_holder.train_input_data, 
+            data_holder.train_input_data.dytpe.names[-1], 
+            0,
+            args.max_depth, 
+            set()
+        )
 
     #Here is a recommended way to print the tree to a file
     # with open(print_out, "w") as file:
